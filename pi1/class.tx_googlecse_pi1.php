7b5a70a05cd84e92be1d2987a255082269013c2c
Todd Hossack <todd@tiraki.com>
2015-10-31 22:49:13 +1300

    Removed incompatible require_once call in tx_googlecse_pi1

--- pi1/class.tx_googlecse_pi1.php
+++ pi1/class.tx_googlecse_pi1.php
@@ -27,9 +27,6 @@
  * Hint: use extdeveval to insert/update function index above.
  */
 
-require_once(PATH_tslib.'class.tslib_pibase.php');
-
-
 /**
  * Plugin 'Google custom search results' for the 'googlecse' extension.
  *
